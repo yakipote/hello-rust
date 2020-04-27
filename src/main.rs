337af388a30f73cc766a8dd2624c854ba9b00231
kikuchi-fuminori <yakipote@redimpulz.com>
2020-04-27 09:52:59 +0900

    udp

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::env;
 #[macro_use]
 extern crate log;
 
-// mod tcp_client;
+mod tcp_client;
 mod tcp_server;
-// mod udp_client;
-// mod udp_server;
+mod udp_client;
+mod udp_server;
 fn main() {
-    env::set_var("RUST_LOG","debug");
+    env::set_var("RUST_LOG", "debug");
     env_logger::init();
     let args: Vec<String> = env::args().collect();
     if args.len() != 4 {
@@ -18,25 +18,23 @@ fn main() {
     let role: &str = &args[2];
     let address: &str = &args[3];
 
-    match protocol  {
-       "tcp" => match role{
-           "server" => {
-               tcp_server::serve(address).unwrap_or_else(|e| error!("{}",e));
-           }
-           "client" => {
-               // TODO: TCPクライアントの呼び出し
-           }
-           _ => {
-               // missing_role();
-           }
-       },
-        "udp" => match role {
+    match protocol {
+        "tcp" => match role {
             "server" => {
-                // TODO: UDPサーバの呼び出し
+                tcp_server::serve(address).unwrap_or_else(|e| error!("{}", e));
             }
             "client" => {
-                // TODO: UDPクライアントの呼び出し
+                tcp_client::connect(address).unwrap_or_else(|e| error!("{}", e));
             }
+            _ => {
+                // missing_role();
+            }
+        },
+        "udp" => match role {
+            "server" => {
+                udp_server::serve(address).unwrap_or_else(|e| error!("{}", e));
+            }
+            "client" => udp_client::communicate(address).unwrap_or_else(|e| error!("{}", e)),
             _ => {
                 // missing_role();
             }
